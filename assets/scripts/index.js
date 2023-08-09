1eda4674768406d6b55ac152a989f3e8d5e47803
Mathieu Daix <mathieudaixpro@gmail.com>
2023-08-09 10:15:52 +0200

    [wip] anim on second section

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,6 +10,42 @@ document.addEventListener('DOMContentLoaded', () => {
         setImageAnimation()
 
         window.addEventListener('resize', setHeightOfImage)
+
+        ScrollTrigger.create({
+            trigger: '.content',
+            onUpdate: self => {
+                const progress = self.progress
+
+                gsap.to('.content .grid', { y: -progress * 400, autoAlpha: 1 * (1 - progress * 2) })
+                gsap.to('.content .image div:first-of-type', { scale: 1 + (1.6 - 1) * progress })
+                gsap.to('.content .image div:nth-of-type(2)', {
+                    scale: 1 + (1.7 - 1) * progress,
+                    autoAlpha: 0.8 * (1 - progress)
+                })
+                gsap.to('.content .image div:nth-of-type(3)', {
+                    scale: 1 + (1.8 - 1) * progress,
+                    autoAlpha: 0.6 * (1 - progress)
+                })
+                gsap.to('.content .image div:nth-of-type(4)', {
+                    scale: 1 + (1.9 - 1) * progress,
+                    autoAlpha: 0.4 * (1 - progress)
+                })
+                gsap.to('.content .image div:nth-of-type(5)', {
+                    scale: 1 + (2 - 1) * progress,
+                    autoAlpha: 0.2 * (1 - progress)
+                })
+                gsap.to('.content .image div:nth-of-type(6)', {
+                    scale: 1 + (2.1 - 1) * progress,
+                    autoAlpha: 0.1 * (1 - progress)
+                })
+                gsap.to('.content .image div:nth-of-type(7)', {
+                    scale: 1 + (2.2 - 1) * progress,
+                    autoAlpha: 0.05 * (1 - progress)
+                })
+            },
+            start: 'top top',
+            end: 'bottom top'
+        })
     }
 })
 
--- blocks/lazyblock-content-1/block.php
+++ blocks/lazyblock-content-1/block.php
@@ -1,16 +1,18 @@
 <section class="content">
     <div class="container grid">
-        <p><?= $attributes['overtitle']; ?></p>
+        <p class="overtitle"><?= $attributes['overtitle']; ?></p>
         <h1><?= $attributes['title']; ?></h1>
         <p><?= $attributes['text']; ?></p>
     </div>
     <?php if (isset($attributes['image']['url'])) : ?>
-        <!-- <div class="grid_image">
+        <div class="grid_image">
             <div class="image">
                 <?php for ($i = 1; $i <= 7; $i++): ?>
                     <div style="background-image: url('<?= esc_url($attributes['image']['url']); ?>')"></div>
                 <?php endfor; ?>
             </div>
-        </div> -->
+        </div>
     <?php endif; ?>
 </section>
+
+<div style="height: 150vh; background: red;"><p>lorem</p></div>
