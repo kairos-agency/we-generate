298a734e568492b2849b4cf26ba4af6a3d0ed0d8
Mathieu Daix <mathieudaixpro@gmail.com>
2023-08-17 15:35:42 +0200

    add legal page

---
+++ page-legal.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Legal
+ */
+?>
+
+<?php get_header(); ?>
+
+<section class="legal">
+    <div class="container">
+        <?php the_content(); ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
