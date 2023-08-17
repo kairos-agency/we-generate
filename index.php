13b0b5b14f6098bd928ee5a84ce2cfaa974e3f48
Gaëtan Jestin <gaetanjestin@outlook.com>
2023-08-17 14:02:49 +0200

    feat: started styling on ecostill page

--- index.php
+++ index.php
@@ -3,21 +3,92 @@
 <header class="hero js-model">
     <div class="container grid">
         <h1>Next Level <em>Cannabis Production technology</em></h1>
-        <p>Regenerative Biomass Microfactory<br/>Optimizes Operations with Circular Production and Upcycling<br/>Maximize Profitability • Revitalize Ecosystem • Support Community</p>
+        <p class="subtitle">Regenerative Biomass Microfactory<br />Optimizes Operations with Circular Production and Upcycling<br />Maximize Profitability • Revitalize Ecosystem • Support Community</p>
         <div>
             <p>EcoStill</p>
             <p>XL</p>
         </div>
     </div>
 
-    <div class="model">
+    <!-- <div class="model">
         <div>
             <canvas id="model"></canvas>
             <span class="circle"></span>
         </div>
-    </div>
+    </div> -->
 </header>
 
-<section class="section" style="background: yellow;"></section>
+<!-- <section class="section" style="background: yellow;"></section> -->
+
+<section class="section">
+    <div class="container grid">
+        <div class="content">
+            <div>
+                <p class="overtitle">What it is</p>
+                <h2>Regenerative Biomass Microfactory</h2>
+                <p class="subtitle">Ethanol Microdistillery • CO2 Generator • Power Plant • Fertilization Unit</p>
+            </div>
+            <div class="cards">
+                <div class="card">
+                    <p>Compact</p>
+                </div>
+                <div class="card">
+                    <p>Automated</p>
+                </div>
+                <div class="card">
+                    <p>Multifonctional</p>
+                </div>
+                <div class="card">
+                    <p>Circular</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section">
+    <div class="container grid">
+        <div class="content">
+            <div>
+                <p class="overtitle">turnkey operational upgrade</p>
+                <h2>Complete Biomass Upcycling</h2>
+                <p class="subtitle">Multiple Commercial Outputs </p>
+            </div>
+            <div class="comparator">
+                <div>
+                    <h3><strong>Ethanol</strong> <br />Microdistillery</h3>
+                    <p><strong>Produces</strong> premium bio-ethanol.</p>
+                    <bl>
+                        <li>From Starch or Sugar</li>
+                        <li>From Cellulose</li>
+                    </bl>
+                </div>
+                <div>
+                    <h3><strong>CO2 Gas</strong> <br />Generator</h3>
+                    <p><strong>Coproduces & Collects</strong> carbon dioxide.</p>
+                    <bl>
+                        <li>Crop Fertilization</li>
+                        <li>Commodity Resale</li>
+                    </bl>
+                </div>
+                <div>
+                    <h3><strong>Biochar</strong> <br />power Plant</h3>
+                    <p><strong>Generates</strong> heat and biochar.</p>
+                    <bl>
+                        <li>Gasifier</li>
+                        <li>Oil Heater</li>
+                    </bl>
+                </div>
+                <div>
+                    <h3><strong>Plant Waste</strong> <br />Upcycler</h3>
+                    <p><strong>Regenerates</strong> biomass completely.</p>
+                    <bl>
+                        <li>Upcycles New Products</li>
+                        <li>Disposes Costly Harmful Waste</li>
+                    </bl>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
