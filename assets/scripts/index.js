ad61f54e9b9192ba2b0ef221af978eb01ab74322
Mathieu Daix <mathieudaixpro@gmail.com>
2023-08-18 10:29:04 +0200

    update image anim on home

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,56 +11,49 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('.slides')) new Slider()
 
     if (window.matchMedia('(min-width: 700px)').matches && document.querySelectorAll('.js-image')) {
-        setHeightOfImage()
         setImageAnimation()
-
-        window.addEventListener('resize', setHeightOfImage)
     }
 })
 
-function getAbsoluteHeight(el) {
-    el = typeof el === 'string' ? document.querySelector(el) : el
-
-    let styles = window.getComputedStyle(el)
-    let margin = parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom'])
-
-    return Math.ceil(el.offsetHeight + margin)
-}
-
-function setHeightOfImage() {
-    document.querySelectorAll('.js-image').forEach(element => {
-        const heroGridHeight = getAbsoluteHeight(element.querySelector('.grid'))
-        element.querySelector('.grid_image').style.height = `calc(100% - ${heroGridHeight}px)`
-    })
-}
-
 function setImageAnimation() {
     document.querySelectorAll('.js-image').forEach(element => {
+        const gridImage = element.querySelector('.grid_image')
+        const top = gridImage.offsetTop
+
         ScrollTrigger.create({
             trigger: element,
             onUpdate: self => {
                 const progress = self.progress
 
+                if (gridImage.getBoundingClientRect().top < window.innerHeight / 2 - gridImage.offsetHeight / 2) {
+                    gridImage.classList.add('sticky')
+                }
+
+                if (gridImage.offsetTop < top + 224) {
+                    gridImage.classList.remove('sticky')
+                }
+
                 gsap.to(element.querySelector('.grid'), { y: -progress * 400, autoAlpha: 1 * (1 - progress * 2) })
 
-                gsap.to(element.querySelector('.image .image_paralax'), { scale: 1 + (1.6 - 1) * progress })
-                gsap.to(element.querySelector('.image .image_paralax img:nth-of-type(2)'), { y: -progress * 40 })
-                gsap.to(element.querySelector('.image .image_paralax img:last-of-type'), {
-                    scale: 1 + (1.2 - 1) * progress,
-                    y: -progress * 50
-                })
+                if (element.querySelector('.image .image_paralax')) {
+                    gsap.to(element.querySelector('.image .image_paralax'), { scale: 1 + (1.8 - 1) * progress })
+                    gsap.to(element.querySelector('.image .image_paralax img:nth-of-type(2)'), { y: -progress * 40 })
+                    gsap.to(element.querySelector('.image .image_paralax img:last-of-type'), {
+                        scale: 1 + (1.3 - 1) * progress,
+                        y: -progress * 50
+                    })
+                }
 
-                gsap.to(element.querySelector('.image div:nth-of-type(2)'), { scale: 1 + (1.7 - 1) * progress * 1.05 })
-                gsap.to(element.querySelector('.image div:nth-of-type(3)'), { scale: 1 + (1.8 - 1) * progress * 1.1 })
-                gsap.to(element.querySelector('.image div:nth-of-type(4)'), { scale: 1 + (1.9 - 1) * progress * 1.15 })
-                gsap.to(element.querySelector('.image div:nth-of-type(5)'), { scale: 1 + (2 - 1) * progress * 1.2 })
-                gsap.to(element.querySelector('.image div:nth-of-type(6)'), { scale: 1 + (2.1 - 1) * progress * 1.25 })
-                gsap.to(element.querySelector('.image div:nth-of-type(7)'), { scale: 1 + (2.2 - 1) * progress * 1.3 })
-                gsap.to(element.querySelector('.image div:nth-of-type(8)'), { scale: 1 + (2.3 - 1) * progress * 1.35 })
+                gsap.to(element.querySelector('.image div:nth-of-type(2)'), { scale: 1 + (1.8 - 1) * progress * 1.05 })
+                gsap.to(element.querySelector('.image div:nth-of-type(3)'), { scale: 1 + (1.9 - 1) * progress * 1.1 })
+                gsap.to(element.querySelector('.image div:nth-of-type(4)'), { scale: 1 + (2 - 1) * progress * 1.15 })
+                gsap.to(element.querySelector('.image div:nth-of-type(5)'), { scale: 1 + (2.1 - 1) * progress * 1.2 })
+                gsap.to(element.querySelector('.image div:nth-of-type(6)'), { scale: 1 + (2.2 - 1) * progress * 1.25 })
+                gsap.to(element.querySelector('.image div:nth-of-type(7)'), { scale: 1 + (2.3 - 1) * progress * 1.3 })
+                gsap.to(element.querySelector('.image div:nth-of-type(8)'), { scale: 1 + (2.4 - 1) * progress * 1.35 })
             },
             start: 'top top',
-            end: 'bottom top',
-            pin: true
+            end: 'bottom top'
         })
     })
 }
--- index.php
+++ index.php
@@ -9,6 +9,20 @@
         <div class="subtitle">
             <p>We empower society to realize a new economic model of prosperity: The Vitality Economy.<br/>We engineer long term prosperity through human and environmental well-being. <br/>Enabling investors and instigators to reform industry for ecosocial symbiosis.<br/>Equipping inventors and entrepreneurs with commercially superior technology.  <br/>Empowering communities with manageable resource autonomy.</p>
         </div>
+        <div class="grid-3">
+            <div>
+                <h3>Reinvent Industry.</h3>
+                <p class="subtitle">Design regenerative solutions that outperform extractive practices and </p>
+            </div>
+            <div>
+                <h3>Restore Environment.</h3>
+                <p class="subtitle">Circular economies support ecosystems by closing the loop on linear supply chains.</p>
+            </div>
+            <div>
+                <h3>Revitalize Society.</h3>
+                <p class="subtitle">Sovereign means of production and supply chains for communities.</p>
+            </div>
+        </div>
     </div>
     <div class="grid_image">
         <div class="image">
