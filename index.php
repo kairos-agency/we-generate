9b83734d68064cdf0824839df2fe6b3d2b0f3bfb
Mathieu Daix <mathieudaixpro@gmail.com>
2023-08-18 11:19:38 +0200

    [wip] section green

--- index.php
+++ index.php
@@ -26,6 +26,9 @@
     </div>
     <div class="grid_image">
         <div class="image">
+            <div class="image_paralax">
+                <img src="http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg" alt="">
+            </div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
@@ -37,16 +40,36 @@
     </div>
 </section>
 
-<section class="content-home js-image">
+<section class="content-home js-image image-green">
     <div class="container grid">
         <p class="overtitle">How we do it</p>
         <h2>Design. Develop. Deploy.</h2>
         <div class="subtitle">
             <p>We Make Things Regenerative. <br/>Upgrade wasteful linear production to profitable circular systems.<br/>Develop ventures to capitalize upon regenerative opportunities.<br/>Leverage natural processes to fuel more efficient operations.</p>
         </div>
+        <div>
+            <h3>3 Operating Levels</h3>
+            <div class="comparator">
+                <div>
+                    <h3><strong>WeGenesis</strong> <br />Meta Studio</h3>
+                    <p class="subtitle">Geotarget opportunities.<br/>Design circular systems.<br/>Develop partnerships.</p>
+                </div>
+                <div>
+                    <h3><strong>WeGenerator</strong> <br />Venture Builder</h3>
+                    <p class="subtitle">Incubate new businesses.<br/>Chain supply symbiotically.<br/>Achieve resource autonomy.</p>
+                </div>
+                <div>
+                    <h3><strong>WeGen</strong> <br />Eco technologies</h3>
+                    <p class="subtitle">Regenerative solutions that<br/>upgrade performance and<br/>maximize profitability.</p>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="grid_image">
         <div class="image">
+            <div class="image_paralax">
+                <img src="http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg" alt="">
+            </div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
@@ -54,6 +77,30 @@
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
             <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+        </div>
+    </div>
+</section>
+
+<section class="content-home content-green">
+    <div class="container grid">
+        <p class="overtitle">When to engage us</p>
+        <h2>Invest. Invent. Incubate. </h2>
+        <div class="subtitle">
+            <p>Uniting investors with inventors and founders.<br/>Empowering entrepreneurs with superior solutions.<br/>Cultivating economies that are better by, and for, Nature.</p>
+        </div>
+        <div class="grid-3 grid-3-border">
+            <div>
+                <h3>Invest</h3>
+                <p class="subtitle">Discover opportunities to to promote our ventures.</p>
+            </div>
+            <div>
+                <h3>Incubate</h3>
+                <p class="subtitle">Develop business ventures to deploy commercially superior eco solutions.</p>
+            </div>
+            <div>
+                <h3>Implement</h3>
+                <p class="subtitle">Integrate regenerative tech to upgrade operations.</p>
+            </div>
         </div>
     </div>
 </section>
