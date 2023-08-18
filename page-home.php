782dc3acea77003c416a22c95211ae29e84d4a56
Mathieu Daix <mathieudaixpro@gmail.com>
2023-08-18 11:54:28 +0200

    update home

---
+++ page-home.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Template Name: Home
+ */
+?>
+
+<?php get_header(); ?>
+
+<?php the_content(); ?>
+
+<section class="content-home js-image">
+    <div class="container grid">
+        <p class="overtitle">What we do</p>
+        <h2>We Regenerize</h2>
+        <div class="subtitle">
+            <p>We empower society to realize a new economic model of prosperity: The Vitality Economy.<br/>We engineer long term prosperity through human and environmental well-being. <br/>Enabling investors and instigators to reform industry for ecosocial symbiosis.<br/>Equipping inventors and entrepreneurs with commercially superior technology.  <br/>Empowering communities with manageable resource autonomy.</p>
+        </div>
+        <div class="grid-3">
+            <div>
+                <h3>Reinvent Industry.</h3>
+                <p class="subtitle">Design regenerative solutions that outperform extractive practices and </p>
+            </div>
+            <div>
+                <h3>Restore Environment.</h3>
+                <p class="subtitle">Circular economies support ecosystems by closing the loop on linear supply chains.</p>
+            </div>
+            <div>
+                <h3>Revitalize Society.</h3>
+                <p class="subtitle">Sovereign means of production and supply chains for communities.</p>
+            </div>
+        </div>
+    </div>
+    <div class="grid_image">
+        <div class="image">
+            <div class="image_paralax">
+                <img src="http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg" alt="">
+            </div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+        </div>
+    </div>
+</section>
+
+<section class="content-home js-image image-green">
+    <div class="container grid">
+        <p class="overtitle">How we do it</p>
+        <h2>Design. Develop. Deploy.</h2>
+        <div class="subtitle">
+            <p>We Make Things Regenerative. <br/>Upgrade wasteful linear production to profitable circular systems.<br/>Develop ventures to capitalize upon regenerative opportunities.<br/>Leverage natural processes to fuel more efficient operations.</p>
+        </div>
+        <div>
+            <h3>3 Operating Levels</h3>
+            <div class="comparator">
+                <div>
+                    <h3><strong>WeGenesis</strong> <br />Meta Studio</h3>
+                    <p class="subtitle">Geotarget opportunities.<br/>Design circular systems.<br/>Develop partnerships.</p>
+                </div>
+                <div>
+                    <h3><strong>WeGenerator</strong> <br />Venture Builder</h3>
+                    <p class="subtitle">Incubate new businesses.<br/>Chain supply symbiotically.<br/>Achieve resource autonomy.</p>
+                </div>
+                <div>
+                    <h3><strong>WeGen</strong> <br />Eco technologies</h3>
+                    <p class="subtitle">Regenerative solutions that<br/>upgrade performance and<br/>maximize profitability.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="grid_image">
+        <div class="image">
+            <div class="image_paralax">
+                <img src="http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg" alt="">
+            </div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-3.jpg')"></div>
+        </div>
+    </div>
+</section>
+
+<section class="content-home content-green">
+    <div class="container grid">
+        <p class="overtitle">When to engage us</p>
+        <h2>Invest. Invent. Incubate. </h2>
+        <div class="subtitle">
+            <p>Uniting investors with inventors and founders.<br/>Empowering entrepreneurs with superior solutions.<br/>Cultivating economies that are better by, and for, Nature.</p>
+        </div>
+        <div class="grid-3 grid-3-border">
+            <div>
+                <h3>Invest</h3>
+                <p class="subtitle">Discover opportunities to to promote our ventures.</p>
+            </div>
+            <div>
+                <h3>Incubate</h3>
+                <p class="subtitle">Develop business ventures to deploy commercially superior eco solutions.</p>
+            </div>
+            <div>
+                <h3>Implement</h3>
+                <p class="subtitle">Integrate regenerative tech to upgrade operations.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content-green content-green-text">
+    <div class="container">
+        <h2>WeGenerate functions in three capacities :</h2>
+        <div class="grid-3">
+            <div>
+                <h3>Incubation</h3>
+                <p class="subtitle">WeGenerator<br/>Eco-Venture Development</p>
+            </div>
+            <div>
+                <h3>Investment</h3>
+                <p class="subtitle">WeGenesis<br/>Eco-Investment</p>
+            </div>
+            <div>
+                <h3>Engineering</h3>
+                <p class="subtitle">WeGen<br/>Eco-Technologies</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content-green content-green-text js-image image-green-top">
+    <div class="container grid">
+    <h2>Envisioning industry that is better by Nature.<br/>Engaging investors with cutting edge innovation.<br/>Equipping founders with leading eco technology.</h2>
+    </div>
+    <div class="grid_image">
+        <div class="image">
+            <div class="image_paralax">
+                <img src="http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg" alt="">
+            </div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+            <div class="image_bg" style="background-image: url('http://localhost:8888/we-generate/wp-content/uploads/2023/08/content-2.jpg')"></div>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
