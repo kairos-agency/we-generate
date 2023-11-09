b8a488426700e54a57b5536ee1452bf6f10a55ff
Mathieu Daix <mathieudaixpro@gmail.com>
2023-11-09 10:22:13 +0100

    add dynamisation of contact page

---
+++ blocks/lazyblock-content-9/block.php
@@ -0,0 +1,8 @@
+<header class="hero-contact js-page-contact">
+    <div class="container">
+        <h1 anim-title><?= $attributes['title']; ?></h1>
+        <div anim-scale>
+            <?= do_shortcode($attributes['shortcode']); ?>
+        </div>
+    </div>
+</header>
